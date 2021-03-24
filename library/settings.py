c7fc806028f79138f9698c7659730bd72b457913
nilton-medeiros <nilton@sistrom.com.br>
2021-03-24 15:25:35 -0300

    Implementaçao do S3 Bucket

--- library/settings.py
+++ library/settings.py
@@ -128,3 +128,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = 'AKIASCWUONU5NVE4LM43'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'bity-library-ngm'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
